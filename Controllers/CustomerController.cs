65f4a680d3ea498cf3ba429de3257f553bfa9552
P4rpleSky <p4rplesky@mail.ru>
2022-08-15 15:26:40 +0300

    LInks added

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text.RegularExpressions;
+
+namespace MarrubiumShop.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Main(string page)
+        {
+            if (Regex.IsMatch(page, @"[A-Z]"))
+                return new NotFoundResult();
+            else
+                return View(page);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -12,4 +12,9 @@ app.MapControllerRoute(
     pattern: "{page?}",
     defaults: new { controller = "Home", action = "Main" });
 
+app.MapControllerRoute(
+    name: "customer",
+    pattern: "my/{page?}",
+    defaults: new { controller = "Customer", action = "Main" });
+
 app.Run();
