4fca02eeda12b92ad55ddb31ceea437f2646aa36
P4rpleSky <p4rplesky@mail.ru>
2022-08-16 20:42:53 +0300

    HTML files refactoring

---
+++ wwwroot/js/view_getters/show_footer.js
@@ -0,0 +1,45 @@
+﻿//<div class="product-section">
+//        <div class="visual">
+//            <a id="p1_1" href="#" class="product-preview"></a>
+//            <a class="set-like favourite"></a>
+//        </div>
+//        <a class="s2" href="#">Крем с ретинолом</a>
+//        <p class="s3">1000 р</p>
+//</div>
+
+show_footer()
+
+function show_footer() {
+
+    const footer = document.createElement("div");
+    footer.className += "container";
+
+    footer.innerHTML = `
+    <div class="row">
+        <div class="col-md-3">
+            <img src="img/logo.png" alt="Marrubium" class="footer-center">
+        </div>
+        <div class="offset-md-1 col-md-2">
+            <div class="footer-menu-1 flex-cont">
+                <a href="https://localhost:7199/about">О нас</a>
+                <a href="https://localhost:7199/catalog">Каталог</a>
+                <a href="https://localhost:7199/contacts">Контакты</a>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="footer-menu-2 flex-cont">
+                <a href="https://localhost:7199/delivery">Доставка</a>
+                <a href="mailto:aliev.af@edu.spbstu.ru">Напишите нам</a>
+            </div>
+        </div>
+        <div class="col-md-3 ">
+            <div class="tel-number footer-center">
+                <p class="tel">+7(999)999-99-99</p>
+            </div>
+        </div>
+    </div>`;
+
+    const footer_place = document.querySelector("div.footer");
+    
+    footer_place.append(footer);
+}
---
+++ wwwroot/js/view_getters/show_header.js
@@ -0,0 +1,51 @@
+﻿//<div class="product-section">
+//        <div class="visual">
+//            <a id="p1_1" href="#" class="product-preview"></a>
+//            <a class="set-like favourite"></a>
+//        </div>
+//        <a class="s2" href="#">Крем с ретинолом</a>
+//        <p class="s3">1000 р</p>
+//</div>
+
+show_header()
+
+function show_header() {
+
+    const header = document.createElement("div");
+    header.className += "container";
+
+    header.innerHTML = `
+    <div class="row">
+        <div class="col-md-2">
+            <a class="logo" href="https://localhost:7199/"></a>
+        </div>
+        <div class="col-md-8">
+            <ui class="main_menu">
+                <li><a class="s1" href="https://localhost:7199/about">О нас</a></li>
+                <li>
+                    <a class="s1" href="https://localhost:7199/catalog">Каталог</a>
+                    <ul>
+                        <li><a class="s2" href="https://localhost:7199/catalog">Крема</a></li>
+                        <li><a class="s2" href="https://localhost:7199/catalog">Сыворотки</a></li>
+                        <li><a class="s2" href="https://localhost:7199/catalog">Очищающие средства</a></li>
+                        <li><a class="s2" href="https://localhost:7199/catalog">Маски</a></li>
+                    </ul>
+                </li>
+                <li><a class="s1" href="https://localhost:7199/delivery">Доставка</a></li>
+                <li><a class="s1" href="https://localhost:7199/contacts">Контакты</a></li>
+            </ui>
+        </div>
+        <div class="col-md-2">
+            <div class="buttons_menu">
+                <a class="profile" href="https://localhost:7199/my/profile"></a>
+                <a class="favourite" href="https://localhost:7199/my/favourite"></a>
+                <a class="cart" href="https://localhost:7199/my/cart"></a>
+                <a class="search" href="#"></a>
+            </div>
+        </div>
+    </div>`;
+
+    const header_place = document.querySelector("header.header");
+    
+    header_place.append(header);
+}
